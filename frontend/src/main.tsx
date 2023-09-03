5eb8160fbfa52cfa3ab4a409cb916df3a9fc0a01
Mohamed <hmohamed@namlatic.com>
2023-09-03 08:47:12 +0100

    prod fixes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,10 +5,22 @@ import "./index.css";
 import { Provider } from "react-redux";
 import {store} from "../store.ts";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>
+// );
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
 );
+
